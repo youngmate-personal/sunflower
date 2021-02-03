30e8880ef83467c795f2150938edeba63a480f85
zhaoyanqi <youngmate@126.com>
2021-02-03 14:40:31 +0800

    feature-insertionSort

---
+++ youngmate-algorithm/src/main/java/com/youngmate/sort/InsertionSort.java
@@ -0,0 +1,18 @@
+package com.youngmate.sort;
+
+/**
+ * @author ZhaoYanqi
+ * @date 2021/2/3 0003
+ */
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+        // 基本情况
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        // 插入过程：默认第一个元素时有序的，从第二个元素开始遍历，和前面的元素进行比较，当遍历
+
+    }
+
+}
