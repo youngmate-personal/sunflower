d39e2323248805f319258cfcaf4fa24b3ef60b60
zhaoyanqi <youngmate@126.com>
2021-03-08 12:04:12 +0800

    feature-insertionSort

--- youngmate-algorithm/src/main/java/com/youngmate/sort/InsertionSort.java
+++ youngmate-algorithm/src/main/java/com/youngmate/sort/InsertionSort.java
@@ -1,5 +1,9 @@
 package com.youngmate.sort;
 
+import java.util.Arrays;
+
+import com.youngmate.sort.util.SortUtils;
+
 /**
  * @author ZhaoYanqi
  * @date 2021/2/3 0003
@@ -11,8 +15,27 @@ public class InsertionSort {
         if (arr == null || arr.length < 2) {
             return;
         }
-        // 插入过程：默认第一个元素时有序的，从第二个元素开始遍历，和前面的元素进行比较，当遍历
+        // 插入过程：默认第一个元素时有序的，从第二个元素开始遍历，和前面的元素依次进行比较，遇到小于前面元素则进行交换
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        int[] array = SortUtils.generateRandomArray(30, 100);
+        int[] copyArray = SortUtils.copyArray(array);
+        insertionSort(array);
+        Arrays.sort(copyArray);
+        if (!SortUtils.isEqual(array, copyArray)) {
+            System.out.println(Arrays.toString(array));
+            System.out.println(Arrays.toString(copyArray));
+        } else {
+            System.out.println("Nice!");
+        }
     }
 
 }
